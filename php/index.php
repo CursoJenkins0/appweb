1953a7003c2225d6e987277a107ed13be49f478d
CursoJenkins0 <108295331+CursoJenkins0@users.noreply.github.com>
2023-02-13 09:38:17 -0300

    Update index.php

--- php/index.php
+++ php/index.php
@@ -5,8 +5,8 @@ $password = 'maroine';
 $db = 'cdweb';
 $conn = new mysqli($host,$user,$password,$db);
 if($con->connect_error){
-    echo '!!!! faild !!!!!' . $conn->connect_error;
+    echo '!!!! Fallido !!!!!' . $conn->connect_error;
 
 }
-echo ' it s great all is done you have containerized your web app !!! ';
+echo '¡¡¡ Esta es una aplicacion web sencilla para practicar. Saludos!!! ';
 ?>
